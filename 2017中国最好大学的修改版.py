4a6890f5bd472ac197e53ab56b74b48ad84f9d49
loilisxka <43001784+loilisxka@users.noreply.github.com>
2018-11-24 11:16:22 +0800

    对爬取2017中国最好大学排名网站的爬虫进行了更新
    更新日志（手动滑稽）：
    在获取排名时，进行了限定，按照人为输入的长度进行爬取，减少了程序的内存占用
    在打印排名时，使用中文空格来进行填充，时排名数据看起来更加整齐

--- 2017中国最好大学的修改版.py
+++ 2017中国最好大学的修改版.py
@@ -7,32 +7,37 @@ def getedhtml(url, code='utf-8'):
     kv = {'user-agent': 'Mozilla/5.0'}#网站有反爬虫措施，需要改变headers，来伪装自己
     try:
         r = requests.get(url, headers=kv, timeout=30)
-        r.raise_for_status()
+        r. raise_for_status()
         r.encoding = code
         return r.text
     except:
         return ''
 
-def returned(html, list):
+def returned(html, list, num):
+    count = 0                                           #对加入列表中的信息进行计数
     soup = BeautifulSoup(html, 'html.parser')
-    for tr in soup.find('tbody', 'hidden_zhpm').children:#寻找其中的tbody标签
+    info = soup.find('tbody', 'hidden_zhpm').children
+    for tr in info:
+        if count >= num:
+            break                                       #如果以及满足所需要的高校数，就可以退出了
         if isinstance(tr, bs4.element.Tag):
+            count += 1
             tds = tr.find_all('td')
             list.append([tds[0].contents[0], tds[1].string, tds[3].string])
-    #这里的tds[0]实际是一个包含了很多节点的父亲节点，所以取出其中的第一项就是我们需要的排名
 
-def printlist(list, num):
-    print('{:^10}{:^10}{:^10}'.format('排名', '大学', '分数'))
+def printed(list, num):
+    print('{0:^10}\t{1:{3}^10}\t{2:^10}'.format('排名', '高校', '分数', chr(12288)))
     for i in range(num):
         L = list[i]
-        print('{:^10}{:^10}{:^10}'.format(L[0], L[1], L[2]))
+        print('{0:^10}\t{1:{3}^10}\t{2:^10}'.format(L[0], L[1], L[2], chr(12288)))
+
 
 def main():
-    L = []
+    list = []
     url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2017.html'
-    num = int(input('您要查找前几名的大学：'))
+    num = int(input('请问要查询2017前多少名的高校呢：'))
     html = getedhtml(url)
-    returned(html, L)
-    printlist(L, num)
+    returned(html, list, num)
+    printed(list, num)
 
 main()
